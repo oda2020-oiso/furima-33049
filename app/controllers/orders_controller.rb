bb6c23a196f7be00ef6e5d93d421d79f83078346
Kentaro Oda <ktr810518@gmail.com>
2021-02-18 00:37:14 +0900

    id取れた

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,14 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: :index
+
   def index
+    @item = Item.find(params[:item_id])
+  end
+
+  private
+
+  def item_params
+    params.require(:item).merge(item_id: params[:item_id])
   end
 
 end
