7dac1128299e2b30283c922d0e8bb40bf47314d5
Kentaro Oda <ktr810518@gmail.com>
2021-02-17 20:08:04 +0900

    クレジットカード機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,18 +2,28 @@ class OrdersController < ApplicationController
 before_action :authenticate_user!, only: :create
 
   def create
-    @order = Order.create(order_params)
+    @order = Order.new(order_params)
     if @order.valid?
       @order.save
       return redirect_to root_path
     else
-      render :index
+      render 'index'
     end
   end
 
   private
 
   def order_params
-    params.require.merge( user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order).merge(user_id: current_user.id, item_id: params[:item.id], token: params[:token])
   end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: order_params[:price],
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,8 @@
 class Order < ApplicationRecord
+  attr_accessor :token
+  validates :token, presence: true
   belongs_to :user
   belongs_to :item
   has_one    :distribution
+
 end
